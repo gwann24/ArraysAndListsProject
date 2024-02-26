780e64b03aaef976d53759d44fd89385fe461463
Greg Wanner <greg@tegogroup.com>
2024-02-26 08:10:58 -0500

    Complete Exercise 2 - Arrays and Lists

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
 
 namespace ArraysAndLists
 {
@@ -9,17 +11,22 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int array and populate it with numbers 1-10
-            
+            var numbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = i + 1;
+            };
 
             /* Create a list of type int
              * Name the list "evens"
              */
-
+            var evens = new List<int>();
             
             /* Create another list of type int
              * Name the list "odds"
              */
             
+            var odds = new List<int>();
 
              /* Using either a foreach or for loop,
              * iterate through the array you populated with 10 numbers.
@@ -29,16 +36,33 @@ namespace ArraysAndLists
              * If the number is odd, add it to the odds list.
              */
 
-
+            foreach (int x in numbers)
+            {
+                if (x % 2 == 0)
+                {
+                    evens.Add(x);
+                } else
+                {
+                    odds.Add(x);
+                }
+            };
 
             /* Using a foreach loop,
              * display the numbers in your "evens" list
              */
             
+            foreach (int y in evens)
+            {
+                Console.WriteLine(y);
+            };
 
             /* Using a for loop,
              * display the numbers in your "odds" list
              */
+            foreach (int z in odds)
+            {
+                Console.WriteLine(z);
+            };
         }
     }
 }
